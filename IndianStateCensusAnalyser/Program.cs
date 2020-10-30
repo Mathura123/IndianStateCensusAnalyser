d01d5d805ece25465c429d2342b9646568df9079
Mathura <mathuradas333@gmail.com>
2020-10-30 10:09:25 +0530

    [Mathura]Add. Welcome message in main branch

---
+++ IndianStateCensusAnalyser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian States Census Analzer Problem Assignment");
+            Console.WriteLine("==========================================================");
+        }
+    }
+}
